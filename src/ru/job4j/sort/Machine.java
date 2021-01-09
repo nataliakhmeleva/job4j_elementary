1093c95e42caf6c0ec411c7b0669c22450a06a39
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-01-09 18:14:05 +0500

    6.9. Жадный алгоритм. Сдача в кофе машине [#441856]

--- src/ru/job4j/sort/Machine.java
+++ src/ru/job4j/sort/Machine.java
@@ -12,7 +12,7 @@ public class Machine {
         for (int i = 0; i < coins.length; i++) {
             while (changeFromShop >= coins[i]) {
                 changeFromShop -= coins[i];
-                rsl[i] = coins[i];
+                rsl[size] = coins[i];
                 size++;
             }
         }
