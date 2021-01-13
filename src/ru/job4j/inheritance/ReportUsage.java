6dddadc129c246e09ac23ec69ac6b08813419229
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-01-13 15:33:14 +0500

    5. Аннотация @Override [#442062]

--- src/ru/job4j/inheritance/ReportUsage.java
+++ src/ru/job4j/inheritance/ReportUsage.java
@@ -3,8 +3,11 @@ package ru.job4j.inheritance;
 public class ReportUsage {
     public static void main(String[] args) {
         JSONReport report = new JSONReport();
+        HtmlReport report1 = new HtmlReport();
         String text = report.generate("name", "body");
         System.out.println(text);
+        String text1 = report1.generate("Report's name", "Report's body");
+        System.out.println(text1);
     }
 
 }
