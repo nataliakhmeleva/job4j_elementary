130b7da138650989abf657f716b8f994c161b920
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-01-17 16:38:43 +0500

    1. Чтение из консоли. Класс Scanner. [#441997]

---
+++ src/ru/job4j/io/MagicBall.java
@@ -0,0 +1,18 @@
+package ru.job4j.io;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+        String question = scanner.nextLine();
+        int answer = new Random().nextInt(3);
+        String s = switch (answer) {
+            case 0 -> "Да";
+            case 1 -> "Нет";
+            default -> "Может быть"; };
+        System.out.println(s);
+    }
+}
