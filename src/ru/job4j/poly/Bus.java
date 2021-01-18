b5af520864cc8a4cc7777bc9406ab0e0c78a8bbf
Natalia Khmeleva <nataliya-mar@mail.ru>
2021-01-18 19:22:07 +0500

    3. Интерфейс Input [#441999]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void passengers(int person) {
+
+    }
+
+    @Override
+    public int fill(int fuel) {
+        return 0;
+    }
+}
---
+++ src/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    public void move();
+
+    public void passengers(int person);
+
+    public int fill(int fuel);
+}
